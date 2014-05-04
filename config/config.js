ff7f932eacca12abdde3a767652bcda5d836d958
Greenstick <benacordier@gmail.com>
2014-05-04 16:58:25 -0700

    Version 0.0.3 - Update

--- config/config.js
+++ config/config.js
@@ -12,19 +12,10 @@ var config = {
     db: 'mongodb://localhost/site-2014-development'
   },
 
-  test: {
-    root: rootPath,
-    app: {
-      name: 'site-2014'
-    },
-    port: 3000,
-    db: 'mongodb://localhost/site-2014-test'
-  },
-
   production: {
     root: rootPath,
     app: {
-      name: 'site-2014'
+      name: 'portfolio-benjamin-cordier'
     },
     port: 5000,
     db: 'mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@' + process.env.MONGO_DATABASE
