365f5d638113de922534358bb9584f1aa570da39
Greenstick <benacordier@gmail.com>
2014-09-08 00:56:59 -0400

    Minor Update

---
+++ app/utility/testers.js
@@ -0,0 +1,9 @@
+var Testers = {
+	users: [
+		{username: 'test', password: 'user'},
+		{username: 'user', password: 'test'},
+		{username: 'test', password: 'pass'}
+	]
+}
+
+module.exports = Testers;
